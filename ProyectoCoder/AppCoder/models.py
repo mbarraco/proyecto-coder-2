cfccf524011fa608adc4d8a72b0230025759402a
mariano <marianobarraco@gmail.com>
2022-09-28 19:59:02 -0300

    agrego primer modelo

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
