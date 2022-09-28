8e31102c691a974407520586a7ebfb136642e544
mariano <marianobarraco@gmail.com>
2022-09-28 19:46:38 -0300

    agrego la primer vista

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def mostrar_inicio(request):
+    return HttpResponse("hola mundo!")
--- ProyectoCoder/ProyectoCoder/urls.py
+++ ProyectoCoder/ProyectoCoder/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from AppCoder.views import mostrar_inicio
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("inicio/", mostrar_inicio)
 ]
