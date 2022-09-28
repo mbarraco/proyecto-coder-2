a57c12f42bb9697f2f5c88bdfbd4854b07683b6d
mariano <marianobarraco@gmail.com>
2022-09-28 20:40:25 -0300

    guardo modelo en la base de datos

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -4,6 +4,7 @@ from AppCoder.models import Estudiante
 
 def mostrar_inicio(request):
     estudiante = Estudiante(nombre="Exequiel", apellido="Velazquez", email="eze@hotmail.com")
+    estudiante.save()
     contexto = {"estudiante_1": estudiante}
     return render(request, "AppCoder/inicio.html", contexto)
 
