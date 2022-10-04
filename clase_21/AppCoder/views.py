76136be58e9fac967618c7885847f695a2ab0aee
mariano <marianobarraco@gmail.com>
2022-10-03 21:01:52 -0300

    agrego clase_21

---
+++ clase_21/AppCoder/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+from AppCoder.models import Estudiante
+
+
+def ayuda(request):
+    return render(request, "AppCoder/ayuda.html")
+
+
+def inicio(request):
+    estudiante = Estudiante(nombre="Exequiel", apellido="Velazquez", email="eze@hotmail.com")
+    estudiante.save()
+    contexto = {"estudiante_1": estudiante}
+    return render(request, "AppCoder/inicio.html", contexto)
+
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html") 
+
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html") 
+
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html") 
+
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
+
